29ddd8d4b8b5af860e60fd78d3564bb3497c7bc5
Lucas Al.Cordeiro <lucasalmeida2007cordeiro@gmail.com>
2026-05-19 22:13:57 -0300

    Update ajudante_de_compras.py adicionando imposto

--- ajudante_de_compras.py
+++ ajudante_de_compras.py
@@ -1,6 +1,8 @@
 saldo_usuario = float(input('Produto: '))
 valor_produto = float(input('Valor: '))
 
+if valor_produto>= 100:
+    valor_produto= valor_produto+valor_produto/10
 saldo_restante = saldo_usuario - valor_produto
 
 if saldo_restante >= 0:
